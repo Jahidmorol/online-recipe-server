ecd177e00a012292cfa6bca257f3e47d465fdcea
jahid morol <jahidmorol2@gmail.com>
2023-05-02 14:08:01 +0600

    implant chef data to localhost

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
+
+const chefs = require('./data/chefs.json');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(chefs)
 })
 
 app.listen(port, () => {
