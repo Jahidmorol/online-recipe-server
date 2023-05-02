f613e4fe468428daca632a3e2eb2c68d50f8c6dc
jahid morol <jahidmorol2@gmail.com>
2023-05-02 14:21:58 +0600

    implant input id to get specific data

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
   res.send(chefs)
 })
 
+app.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const chef = chefs.find(chef => chef.id == id)
+  res.send(chef)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
